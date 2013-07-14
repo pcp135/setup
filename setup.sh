68ffcd91fb729123e1736b2f4c1b9d5594d707c5
Phil Parsons <phil@parsons.uk.com>
2013-07-14 23:34:37 +0000

    dumping ssh key to screen

--- setup.sh
+++ setup.sh
@@ -55,3 +55,6 @@ heroku keys:add
 # Set names up on git
 git config --global user.name "Phil Parsons"
 git config --global user.email phil@parsons.uk.com
+
+# Output rsa key to screen for copying into git
+cat ~/.ssh/id_rsa.pub
